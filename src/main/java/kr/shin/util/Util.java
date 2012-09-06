78e4fb7a12eb37c8942867e217aedbc1706bb1f0
ftwinkle <ftwinkle@nate.com>
2012-09-06 14:14:34 +0900

    Util Project Setup - Maven, Junit4 추가 - 소스경로 변경 - Web Project 추가 - Lib Dependency 추가

---
+++ src/main/java/kr/shin/util/Util.java
@@ -0,0 +1,13 @@
+package kr.shin.util;
+
+public class Util {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github Shin's Util Package!");
+	}
+
+}
